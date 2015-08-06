2fad2402130366614d869e1fbdc99a67c3d35ca7
rr1000 <rrichrs@gmail.com>
2015-08-06 09:07:52 -0700

    Initial commit

---
+++ config.rb
@@ -0,0 +1,38 @@
+require 'builder'
+
+activate :directory_indexes
+
+set :css_dir, 'assets/css'
+set :js_dir, 'assets/js'
+set :images_dir, 'assets/img'
+
+Time.zone = "UTC"
+
+# Markdown Settings
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true, :tables => true, :with_toc_data => true, :no_intra_emphasis => true
+activate :syntax, :wrap => true
+
+# Blog Settings
+activate :blog do |blog|
+  blog.prefix = "blog"
+  # blog.permalink = "blog/{year}/{title}.html"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.taglink = "{tag}.html"
+  # blog.layout = "layout_blog"
+  blog.summary_length = 250
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
+end
+
+# Sitemap Settings
+set :url_root, 'XXXXX.com'
+activate :search_engine_sitemap
+page "/sitemap.xml", :layout => false
+page "/feed.xml", layout: false
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :directory_indexes
+end
